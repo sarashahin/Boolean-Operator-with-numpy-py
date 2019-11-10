bc2480d28cb968fbd4047310d22602ed243f0e97
sarashahin <56139073+sarashahin@users.noreply.github.com>
2019-11-10 00:10:56 +0000

    Add files via upload

---
+++ Boolean operators with numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+print(np.__version__)
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+print(np.logical_or(my_house > 18.5, my_house < 10))
+print(np.logical_and(my_house < 11, your_house < 11))
